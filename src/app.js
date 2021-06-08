aff02b125ab235183d6bf4a27e281ffe8deb5864
Carlosanamurov <carlosanamuroventura@upeu.edu.pe>
2021-06-08 11:23:29 -0500

    authtoken

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import morgan from 'morgan'
 import rolesRoutes from './routers/rol.routes'
 import alumnoRouter from './routers/alumno.routers'
+import userRouters from './routers/user.routers'
+import authRouters from './routers/auth.routers'
 
 
 const app= express();
@@ -18,4 +20,7 @@ app.get('/',function(req,res,next) {
 });
     app.use('/api/auth/roles', rolesRoutes);
     app.use('/api/alumno', alumnoRouter);
+    app.use('/api/auth', userRouters);
+    app.use('/api/auth/users', authRouters);
+    
    export default app;
